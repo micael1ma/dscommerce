2d9b5af8f903c71f86e7f6cc63914b4a05f2ccbe
micael1ma <micael1ma.professional@gmail.com>
2025-01-28 23:57:06 -0300

    Equals and hashCode

--- src/main/java/com/micael1ma/dscommerce/entities/OrderItemPk.java
+++ src/main/java/com/micael1ma/dscommerce/entities/OrderItemPk.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPk {
 
@@ -32,5 +34,20 @@ public class OrderItemPk {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPk that = (OrderItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(order);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
     }
 }
