22a6d45d3dc7aff854b56614016b710e6617bf38
micael1ma <micael1ma.professional@gmail.com>
2025-01-31 15:42:38 -0300

    Get Products findAll/findByID

--- src/main/java/com/micael1ma/dscommerce/services/ProductService.java
+++ src/main/java/com/micael1ma/dscommerce/services/ProductService.java
@@ -4,10 +4,12 @@ import com.micael1ma.dscommerce.dto.ProductDTO;
 import com.micael1ma.dscommerce.entities.Product;
 import com.micael1ma.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -20,4 +22,11 @@ public class ProductService {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
 }
