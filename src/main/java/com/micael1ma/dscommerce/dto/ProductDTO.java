0f7fa3124e9c5e424fec95a64a05240d01b07aac
micael1ma <micael1ma.professional@gmail.com>
2025-02-09 02:56:54 -0300

    Bean validation

--- src/main/java/com/micael1ma/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/micael1ma/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,23 @@
 package com.micael1ma.dscommerce.dto;
 
 import com.micael1ma.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço deve ser postivo")
     private Double price;
     private String imgUrl;
 
